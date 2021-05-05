6ee214b101d6c5490d34cc01d6f73da46b7cb98c
Alonzo A <alonzoa2015@gmail.com>
2021-05-05 09:08:27 -0700

    added reset button

--- app.js
+++ app.js
@@ -23,10 +23,12 @@ btn.addEventListener('click', () =>{
     if (guessN > num){
         tooHigh.style.display = 'block';
         tooLow.style.display = 'none';
+        winDiv.style.display = 'none';
     }
     if (guessN < num){
         tooLow.style.display = 'block';
         tooHigh.style.display = 'none';
+        winDiv.style.display = 'none';
     }
     if (guessN === num){
         tooLow.style.display = 'none';
@@ -41,5 +43,7 @@ btn.addEventListener('click', () =>{
 btnR.addEventListener('click', () =>{
     win = 0;
     loss = 0;
-
+    tooLow.style.display = 'none';
+    tooHigh.style.display = 'none';
+    winDiv.style.display = 'none';
 });
