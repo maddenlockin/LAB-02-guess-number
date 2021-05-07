47b802659438a3eec6217344b4230e4f4ffe66a5
maddenlockin <maddenlockin@gmail.com>
2021-05-07 13:08:34 -0700

    refactoring

--- app.js
+++ app.js
@@ -6,19 +6,23 @@ const tooHigh = document.getElementById('too-high');
 const tooLow = document.getElementById('too-low');
 const outDiv = document.getElementById('results');
 const winDiv = document.getElementById('win-div');
+const remainingDiv = document.getElementById('remaining-guesses');
+const guess = document.getElementById('num-input');
 // initialize state
 let win = 0;
 let loss = 0;
+let remaining = 5;
+let num = Math.ceil(Math.random() * 20);
 
 // set event listeners to update state and DOM
 btn.addEventListener('click', () =>{
-    let num = Math.ceil(Math.random() * 20);
-    const guess = document.getElementById('num-input').value;
-    const guessN = Number(guess);
+    const guessN = Number(guess.value);
     if (checkNumber(guessN, num) === true){
         win++;
+        btn.style.display = 'none';
     } else {
         loss++;
+        remaining--;
     }
     if (guessN > num){
         tooHigh.style.display = 'block';
@@ -34,10 +38,15 @@ btn.addEventListener('click', () =>{
         tooLow.style.display = 'none';
         tooHigh.style.display = 'none';
         winDiv.style.display = 'block';
+        outDiv.textContent = `You got the answer in ${win} / ${loss} tries!`;
     }
-    console.log(win, loss);
-    console.log(num);
-    outDiv.textContent = `You've gotten ${win} / ${loss} right!`;
+    if (remaining === 0){
+        btn.style.display = 'none';
+        outDiv.textContent = `You didn't guess correctly, try again!`;
+        tooLow.style.display = 'none';
+        tooHigh.style.display = 'none';
+    }
+    remainingDiv.textContent = `You have ${remaining} guesses remaining`;
 });
 
 btnR.addEventListener('click', () =>{
@@ -46,4 +55,8 @@ btnR.addEventListener('click', () =>{
     tooLow.style.display = 'none';
     tooHigh.style.display = 'none';
     winDiv.style.display = 'none';
+    num = Math.ceil(Math.random() * 20);
+    remaining = 5;
+    btn.style.display = 'block';
+    guess.value = '';
 });
